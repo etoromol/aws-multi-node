9174e1222b6b8ad6db9c8f0fa833385b4abfc6e8
Eduardo Toro <9393928+etoromol@users.noreply.github.com>
2021-07-17 18:24:50 -0500

    Infrastructure ready for testing.

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # aws-multi-node
-Pending.
+Terraform enables you to safely and predictably create, change and improve infrastructures. This allows a blueprint of your datacenter to be versioned and treated as you would any other code. Additionally, infrastuctures can be shared and re-used. 
+
+This project is a custom infrastructure built with [Terraform Language (HCL)](https://www.terraform.io/docs/language/index.html) to deploy a single virtual instance on Amazon Web Services. Besides you will have a pool of instances of common interest that you can customize and deploy based on your needs. 
+
+## Prerequisites
+
+To deploy this infrastructure you will need:
+* The [Terraform CLI](https://learn.hashicorp.com/tutorials/terraform/install-cli?in=terraform/aws-get-started) (0.14.9+) installed.
+* The [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html) installed.
+* [An AWS account](https://aws.amazon.com/free/).
+* Your AWS credentials. You can [create a new Access Key on this page](https://console.aws.amazon.com/iam/home?#/security_credentials).
+
+## Installation
+
+1. Download a copy of the infrastructure to your local device:
+```bash
+git clone https://github.com/etoromol/aws-single-node.git
+cd aws-single-node
+```
+2. Initialize the working directory containing Terraform configuration files:
+```bash
+terraform init
+```
+
+## Configuration
+
+3. Setup the AWS CLI from your terminal. Follow the prompts to input your AWS Access Key ID and Secret Access Key:
+```hcl
+aws configure
+```
+*The configuration process stores your credentials in a file at ~/.aws/credentials on MacOS and Linux, or %UserProfile%\.aws\credentials on Windows.*
+
+4. Go to [variable.tf](variables.tf) file and customize your project's name and environment arguments:
+```hcl
+variable "project" {
+  description = "Name and Environment type of the infrastructure"
+  type        = map(string)
+  default = {
+    "name"        = "aws_sandbox" <-
+    "environment" = "d"           <-
+  }
+}
+```  
+5. Go to [variable.tf](variables.tf) file and define the access_key default argument with the name of your SSH key:
+```hcl
+var "access_key" {
+  description = "Name of the SSH key selected to access the instances"
+  type = string
+  default = "" <-
+}
+```  
+
+## Deployment
+
+6. Once your infrastructure is ready, start with the deployment:
+```bash
+terraform apply
+```
+7. When your instance is ready, access using your ssh key:
+```bash
+ssh -i "path_to_your_ssh_key" ec2-user@public_ip_address_given
+```
+8. Destroy your infrastructure if it is no longer needed:
+
+```bash
+terraform destroy
+```
+*The deployment and destroy plans will show to you by Terraform before and after the initialization. Type yes if you are good, otherwise Terraform will not proceed further.*
+
+## License
+
+Copyright (c) 2021 Eduardo Toro.
+
+Licensed under the [MIT](LICENSE) license.
--- main.tf
+++ main.tf
@@ -21,29 +21,73 @@ provider "aws" {
   region = var.region["uw1"]
 }
 
-# Resource tag name arguments created by interpolating 
+# Resource's tag name arguments created by interpolating 
 # the 'resource name'-'environment value'-'name value'
 # from global variable "project".
-# locals {
-#   vpc_name        = "vpc-${var.project["environment"]}-${var.project["name"]}"
-#   igw_name        = "igw-${var.project["environment"]}-${var.project["name"]}"
-#   seg_name        = "seg-${var.project["environment"]}-${var.project["name"]}"
-#   snt_public_name = "snt-${var.project["environment"]}-${var.project["name"]}"
-#   rtb_public_name = "rtb-${var.project["environment"]}-${var.project["name"]}"
-#   nic_public_name = "nic-${var.project["environment"]}-${var.project["name"]}"
-#   eip_name        = "eip-${var.project["environment"]}-${var.project["name"]}"
-# }
+
+locals {
+  vpc_name         = "vpc-${var.project["environment"]}-${var.project["name"]}"
+  eip_01_name      = "eip_01-${var.project["environment"]}-${var.project["name"]}"
+  eip_02_name      = "eip_02-${var.project["environment"]}-${var.project["name"]}"
+  igw_name         = "igw-${var.project["environment"]}-${var.project["name"]}"
+  ngw_name         = "ngw-${var.project["environment"]}-${var.project["name"]}"
+  snt_public_name  = "snt_public-${var.project["environment"]}-${var.project["name"]}"
+  snt_private_name = "snt_private-${var.project["environment"]}-${var.project["name"]}"
+  seg_public_name  = "seg-${var.project["environment"]}-${var.project["name"]}"
+  seg_private_name = "seg-${var.project["environment"]}-${var.project["name"]}"
+  rta_public_name  = "rta_public-${var.project["environment"]}-${var.project["name"]}"
+  rta_private_name = "rta_private-${var.project["environment"]}-${var.project["name"]}"
+  nic_01_name      = "nic_01-${var.project["environment"]}-${var.project["name"]}"
+  nic_02_name      = "nic_02-${var.project["environment"]}-${var.project["name"]}"
+  nic_03_name      = "nic_03-${var.project["environment"]}-${var.project["name"]}"
+}
 
 resource "aws_vpc" "vpc_multi_node" {
-  cidr_block           = var.netblock["network"]
-  instance_tenancy     = "default"
+  cidr_block       = var.netblock["network"]
+  instance_tenancy = "default"
 
   tags = {
     Name = local.vpc_name
   }
 }
 
-# Subnets
+resource "aws_eip" "eip_01" {
+  depends_on = [aws_internet_gateway.igw_multi_node]
+  vpc        = true
+
+  tags = {
+    "Name" = local.eip_01_name
+  }
+}
+
+resource "aws_eip" "eip_02" {
+  depends_on        = [aws_internet_gateway.igw_multi_node]
+  network_interface = aws_network_interface.nic_01.id
+  vpc               = true
+
+  tags = {
+    "Name" = local.eip_02_name
+  }
+}
+
+resource "aws_internet_gateway" "igw_multi_node" {
+  vpc_id = aws_vpc.vpc_multi_node.id
+
+  tags = {
+    Name = local.igw_name
+  }
+}
+
+resource "aws_nat_gateway" "ngw_multi_node" {
+  depends_on    = [aws_internet_gateway.igw_multi_node]
+  allocation_id = aws_eip.eip_01.id
+  subnet_id     = aws_subnet.snt_public.id
+
+  tags = {
+    Name = local.ngw_name
+  }
+}
+
 resource "aws_subnet" "snt_public" {
   vpc_id            = aws_vpc.vpc_multi_node.id
   cidr_block        = var.netblock["public"]
@@ -60,87 +104,36 @@ resource "aws_subnet" "snt_private" {
   availability_zone = var.region["uw1b"]
 
   tags = {
-    Name = ""
+    Name = local.snt_private_name
   }
 }
 
-# Gateways:
-resource "aws_internet_gateway" "igw_multi_node" {
-  vpc_id = aws_vpc.vpc_multi_node.id
+resource "aws_security_group" "seg_public" {
+  vpc_id      = aws_vpc.vpc_multi_node.id
+  description = "Allow only SSH traffic from any source"
 
-  tags = {
-    Name = local.igw_name
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-resource "aws_nat_gateway" "ngw_multi_node" {
-  depends_on    = [aws_internet_gateway.igw_multi_node]
-  allocation_id = aws_eip.eip_nodes.id
-  subnet_id     = aws_subnet.snt_public.id
-
-    tags = {
-      Name = ""
-  }
-}
-
-# Elastic IPs
-resource "aws_eip" "eip_inband" {
-  depends_on = [aws_internet_gateway.igw_multi_node]
-  vpc        = true
-
-  tags = {
-    "Name" = ""
-  }
-}
-
-resource "aws_eip" "eip_nodes" {
-  depends_on = [aws_internet_gateway.igw_multi_node]
-  vpc        = true
-
-  tags = {
-    "Name" = ""
-  }
-}
-
-resource "aws_route_table" "rta_public" {
-  vpc_id = aws_vpc.vpc_multi_node.id
-
-  route {
-    cidr_block = var.netblock["default"]
-    gateway_id = aws_internet_gateway.igw_multi_node.id
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = ""
+    Name = local.seg_public_name
   }
 }
 
-resource "aws_route_table" "rta_private" {
-  vpc_id = aws_vpc.vpc_multi_node.id
-
-  route {
-    cidr_block     = var.netblock["default"]
-    nat_gateway_id = aws_nat_gateway.ngw_multi_node.id
-  }
-
-  tags = {
-    Name = local.rtb_public_name
-  }
-}
-
-# Associations
-resource "aws_route_table_association" "ass_public" {
-  subnet_id      = aws_subnet.snt_public.id
-  route_table_id = aws_route_table.rta_public.id
-}
-
-resource "aws_route_table_association" "ass_private" {
-  subnet_id      = aws_subnet.snt_private.id
-  route_table_id = aws_route_table.rta_private.id
-}
-
-# Security group
-resource "aws_security_group" "seg_multi_node" {
+resource "aws_security_group" "seg_private" {
   vpc_id      = aws_vpc.vpc_multi_node.id
   description = "Allow only SSH, ICMP, TLS inbound traffic from any source"
 
@@ -177,20 +170,125 @@ resource "aws_security_group" "seg_multi_node" {
   }
 
   tags = {
-    Name = local.seg_name
+    Name = local.seg_private_name
   }
 }
 
-# INSIDE MACHINE
+resource "aws_route_table" "rta_public" {
+  vpc_id = aws_vpc.vpc_multi_node.id
+
+  route {
+    cidr_block = var.netblock["default"]
+    gateway_id = aws_internet_gateway.igw_multi_node.id
+  }
+
+  tags = {
+    Name = local.rta_public_name
+  }
+}
+
+resource "aws_route_table" "rta_private" {
+  vpc_id = aws_vpc.vpc_multi_node.id
+
+  route {
+    cidr_block     = var.netblock["default"]
+    nat_gateway_id = aws_nat_gateway.ngw_multi_node.id
+  }
+
+  tags = {
+    Name = local.rta_private_name
+  }
+}
+
+resource "aws_route_table_association" "ass_public" {
+  subnet_id      = aws_subnet.snt_public.id
+  route_table_id = aws_route_table.rta_public.id
+}
+
+resource "aws_route_table_association" "ass_private" {
+  subnet_id      = aws_subnet.snt_private.id
+  route_table_id = aws_route_table.rta_private.id
+}
+
+resource "aws_network_interface" "nic_01" {
+  subnet_id       = aws_subnet.snt_public.id
+  security_groups = [aws_security_group.seg_public.id]
+  private_ips     = ["10.0.1.117"]
+
+  tags = {
+    Name = local.nic_01_name
+  }
+}
+
+resource "aws_network_interface" "nic_02" {
+  subnet_id       = aws_subnet.snt_private.id
+  security_groups = [aws_security_group.seg_private.id]
+  private_ips     = ["10.0.2.11"]
+
+  tags = {
+    Name = local.nic_02_name
+  }
+}
+
+resource "aws_network_interface" "nic_03" {
+  subnet_id       = aws_subnet.snt_private.id
+  security_groups = [aws_security_group.seg_private.id]
+  private_ips     = ["10.0.2.22"]
+
+  tags = {
+    Name = local.nic_03_name
+  }
+}
+
+# Elastic Cloud Compute Instances:
+#  ec2_00: Amazon Linux 2
+#  ec2_01: cat8000v - 17.4.1b
+#  ec2_02: csr1000v - 17.3.3
+
 resource "aws_instance" "ec2_01" {
   ami               = var.vm0["ami"]
   instance_type     = var.vm0["instance_type"]
   availability_zone = var.vm0["availability_zone"]
   key_name          = var.access_key
-  subnet_id         = aws_subnet.snt_private.id
-  security_groups = [aws_security_group.seg_multi_node.id]
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.nic_01.id
+  }
 
   tags = {
     Name = var.vm0["name"]
+  }
+}
+
+resource "aws_instance" "ec2_02" {
+  ami               = var.vm1["ami"]
+  instance_type     = var.vm1["instance_type"]
+  availability_zone = var.vm1["availability_zone"]
+  key_name          = var.access_key
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.nic_02.id
+  }
+
+  tags = {
+    Name = var.vm1["name"]
+  }
+}
+
+resource "aws_instance" "ec2_03" {
+  ami               = var.vm3["ami"]
+  instance_type     = var.vm3["instance_type"]
+  availability_zone = var.vm3["availability_zone"]
+  key_name          = var.access_key
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.nic_03.id
+  }
+
+  tags = {
+    Name = var.vm3["name"]
   }
 }
